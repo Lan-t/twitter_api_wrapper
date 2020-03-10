c401ff14cc95250e07b81e6c2bb9b9844de59ce3
tia <yknms3533642@yahoo.co.jp>
2020-03-10 22:23:14 +0900

    gunicorn

---
+++ api_keys.py
@@ -0,0 +1,4 @@
+import os
+
+API_KEY = os.environ.get('TWITTER_API_KEY')
+API_SECRET_KEY = os.environ.get('TWITTER_API_KEY_SECRET')
---
+++ gunicorn-config.py
@@ -0,0 +1,25 @@
+import multiprocessing
+import os
+
+name = 'gunicorn'
+
+accesslog = 'accesslog.log'
+errorlog = 'errorlog.log'
+
+bind = 'localhost:8080'
+
+worker_class = "uvicorn.workers.UvicornWorker"
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_connection = '1024'
+backlog = 2048
+max_requests = 5120
+timeout = 120
+keepalive = 2
+
+user = os.environ.get('PYUSER')
+group = os.environ.get('PYGROUP')
+
+debug = os.environ.get('DEBUG', 'false') == 'true'
+reload = debug
+preload_app = False
+daemon = False
